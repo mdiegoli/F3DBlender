bfd184aa86d702a3261deaded5e7177798614434
Michele <morticcino@diegoli.org>
2020-06-24 08:16:28 +0200

    add wip script

---
+++ f3d_draw_curve.py
@@ -0,0 +1,17 @@
+#import bpy
+#access object
+#bpy.data.objects["Cube"].data.vertices[0].co.x += 1.0
+#lista oggetti che sono in scena
+#list(bpy.data.objects)
+#if "Cube" in bpy.data.meshes:
+#    mesh = bpy.data.meshes["Cube"]
+#    print("removing mesh", mesh)
+#    bpy.data.meshes.remove(mesh)
+#accedo alla proprietà degli oggetti in scena
+#bpy.data.objects[0].name
+#operatore dove configurare tutti i valori da impostare a mano per "gonfiare" la curva
+#bpy.ops.curve
+
+################
+#todo
+#creare nuova opt? o nuovo pannello? o add-on?
